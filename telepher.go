69998694e99196d274705474f514391c0b05d59b
RabbitFoRed <73241991+RabbitFored@users.noreply.github.com>
2021-06-19 00:26:08 -0700

    Update telepher.go

--- telepher.go
+++ telepher.go
@@ -3,12 +3,48 @@ package telepher
 import (
   "net/http"
   "time"
+ 
   )
 const (
   DefaultBaseURL = "https://api.telegram.org/bot"
   DefaultBaseFileURL = "https://api.telegram.org/file/bot"
-  DefaultParseMode = "None"
+  DefaultParseMode = ""
+  DefaultCheckBool = true
  )
+
+const DefaultHandlerGroup = 0
+
 var DefaultClient = &http.Client{
   Timeout: time.Second * 10,
 }
+
+type ParseMode = string
+
+var PARSE_MODES = []string{"markdownv2", "html", "markdown",""}
+
+const (
+	Default    ParseMode = ""
+  Markdown   ParseMode = "Markdown"
+	MarkdownV2 ParseMode = "MarkdownV2"
+	HTML       ParseMode = "HTML"
+)
+
+
+// events
+const (
+    Message                    =      "message"
+    EditedMessage              =      "edited_message"
+    ChannelPost                =      "channel_post"
+    EditedChannelPost          =      "edited_channel_post"
+    InlineQuery                =      "inline_query"
+    ChosenInlineResult         =      "chosen_inline_result"
+    CallbackQuery              =      "callback_query"
+    ShippingQuery              =      "shipping_query"
+    PreCheckoutQuery           =      "pre_checkout_query"
+    Poll                       =      "poll"
+    PollAnswer                 =      "inline_query"
+    MyChatMember               =      "my_chat_member"
+    ChatMember                 =      "chat_member"
+    Text                        =      "text"
+
+)
